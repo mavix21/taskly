7fbbed1eaaf43f27c66247d35dacfbec81fb6e4a
mavix21 <marcevizcarra2109@hotmail.com>
2026-01-03 18:38:32 -0500

    feat: configure react native eslint plugin

--- eslint.config.js
+++ eslint.config.js
@@ -2,10 +2,20 @@
 const { defineConfig } = require("eslint/config");
 const expoConfig = require("eslint-config-expo/flat");
 const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended");
+const reactNativePlugin = require("eslint-plugin-react-native");
 
 module.exports = defineConfig([
   expoConfig,
   eslintPluginPrettierRecommended,
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    plugins: {
+      "react-native": reactNativePlugin,
+    },
+    rules: {
+      "react-native/no-unused-styles": "error",
+    },
+  },
   {
     ignores: ["dist/*"],
   },
