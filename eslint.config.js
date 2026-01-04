74af91f98b37042252e4390e4a500cc3c563f1d3
mavix21 <marcevizcarra2109@hotmail.com>
2026-01-03 20:50:55 -0500

    refactor: simplify eslint plugin react native config

--- eslint.config.js
+++ eslint.config.js
@@ -2,15 +2,14 @@
 const { defineConfig } = require("eslint/config");
 const expoConfig = require("eslint-config-expo/flat");
 const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended");
-const reactNativePlugin = require("eslint-plugin-react-native");
+const eslintPluginReactNative = require("eslint-plugin-react-native");
 
 module.exports = defineConfig([
   expoConfig,
   eslintPluginPrettierRecommended,
   {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
     plugins: {
-      "react-native": reactNativePlugin,
+      "react-native": eslintPluginReactNative,
     },
     rules: {
       "react-native/no-unused-styles": "error",
