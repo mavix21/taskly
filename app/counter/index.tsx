6e7d0246a8b399452dc52e805cbe0477b9de8879
mavix21 <marcevizcarra2109@hotmail.com>
2026-01-12 18:28:13 -0500

    feat: add manually scheduled local push notifications

--- app/counter/index.tsx
+++ app/counter/index.tsx
@@ -1,8 +1,36 @@
 import { useRouter } from "expo-router";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Alert, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import * as React from "react";
+import { theme } from "../../theme";
+import { registerForPushNotificationsAsync } from "../../utils/registerForPushNotificationsAsync";
+import * as Device from "expo-device";
+import * as Notifications from "expo-notifications";
 
 export default function CounterScreen() {
   const router = useRouter();
+
+  const schedulePushNotification = React.useCallback(async () => {
+    const result = await registerForPushNotificationsAsync();
+    if (result === "granted") {
+      await Notifications.scheduleNotificationAsync({
+        content: {
+          title: "I'm a notification from your app 📩",
+        },
+        trigger: {
+          type: Notifications.SchedulableTriggerInputTypes.TIME_INTERVAL,
+          seconds: 5,
+          channelId: "default",
+        },
+      });
+    } else {
+      if (Device.isDevice) {
+        Alert.alert(
+          "Unable to schedule notification",
+          "Enable the notification permission for Expo Go in settings",
+        );
+      }
+    }
+  }, []);
 
   return (
     <View style={styles.container}>
@@ -12,6 +40,13 @@ export default function CounterScreen() {
         </Text>
       </TouchableOpacity>
       <Text style={styles.text}>Counter</Text>
+      <TouchableOpacity
+        style={styles.button}
+        activeOpacity={0.8}
+        onPress={schedulePushNotification}
+      >
+        <Text style={styles.buttonText}>Schedule notification</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -22,6 +57,17 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#fff",
+  },
+  button: {
+    backgroundColor: theme.colorBlack,
+    padding: 12,
+    borderRadius: 6,
+  },
+  buttonText: {
+    color: theme.colorWhite,
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    letterSpacing: 1,
   },
   text: {
     fontSize: 24,
