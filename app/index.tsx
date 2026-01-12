c0c6c8521fc1c6268990828889de143ae135be41
mavix21 <marcevizcarra2109@hotmail.com>
2026-01-11 20:07:36 -0500

    feat: add text input to submit new items to the shopping list

--- app/index.tsx
+++ app/index.tsx
@@ -1,24 +1,64 @@
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, TextInput } from "react-native";
+import * as React from "react";
 import { ShoppingListItem } from "../components/ShoppingListItem";
-import { Link } from "expo-router";
+
+type ShoppingListItemType = {
+  id: string;
+  name: string;
+  isCompleted: boolean;
+};
+
+const initialItems: ShoppingListItemType[] = [
+  { id: "1", name: "Coffee", isCompleted: false },
+  { id: "2", name: "Rice", isCompleted: false },
+  { id: "3", name: "Tea", isCompleted: true },
+];
 
 export default function App() {
+  const [shoppingList, setShoppingList] =
+    React.useState<ShoppingListItemType[]>(initialItems);
+  const [value, setValue] = React.useState("");
+
+  const handleSubmit = React.useCallback(() => {
+    if (!value || value.trim() === "") {
+      return;
+    }
+
+    setShoppingList((prev) => [
+      { id: Date.now().toString(), name: value, isCompleted: false },
+      ...prev,
+    ]);
+    setValue("");
+  }, [value]);
+
   return (
     <View style={styles.container}>
-      <Link href="/counter" style={{ textAlign: "center", marginBottom: 10 }}>
-        Go to counter
-      </Link>
-      <ShoppingListItem name="Coffe" />
-      <ShoppingListItem name="Rice" isCompleted={true} />
-      <ShoppingListItem name="Tea" />
+      <TextInput
+        style={styles.textInput}
+        placeholder="E.g. Coffe"
+        value={value}
+        onChangeText={setValue}
+        returnKeyType="send"
+        onSubmitEditing={handleSubmit}
+      />
+      {shoppingList.map((item) => (
+        <ShoppingListItem key={item.id} {...item} />
+      ))}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop: 24,
     flex: 1,
     backgroundColor: "#fff",
-    justifyContent: "center",
+  },
+  textInput: {
+    borderColor: "#eee",
+    borderWidth: 1,
+    padding: 12,
+    marginHorizontal: 16,
+    borderRadius: 9999999,
   },
 });
